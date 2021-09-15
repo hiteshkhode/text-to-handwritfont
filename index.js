a91a22af75b5fa1d601631a51efa182c0b8be16a
Hitesh Khode <khodehitesh@gmail.com>
2021-09-15 10:46:59 +0530

    from windows

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const jimp = require('jimp')
-jimp.read('./testh.jpg').then((img) =>{
+jimp.read('./testh.jpg').then((image) =>{
+    img = image.clone()
     img.greyscale().write('modified.jpg')
     for (let i = 0; i < 31329; i++) {
         if(img.bitmap.data[4*i] < 100) toColor = 0
@@ -8,5 +9,52 @@ jimp.read('./testh.jpg').then((img) =>{
             img.bitmap.data[4*i + index] = toColor;
         }
     }
+    for (let i = 0; i < 31329; i++) {
+        if(img.bitmap.data[4*i] < 100){
+            upperthreshold = i/177
+            // img.crop(0, upperthreshold - 1, 177, 177 - i%177)
+            break
+        }
+    }
+    
+    for (let i = 31329; i > 0; i--) {
+        if(img.bitmap.data[4*i - 2] < 100){
+            lowerthreshold = i/177
+            // img.crop(0, 0, 177, 177 - i%177)
+            break
+        }
+    }
+    // console.log(img.bitmap.data)
+    // for (let i = 0; i < img.bitmap.width; i++) {
+    //     for (let j = 0; j < img.bitmap.height; j++) {
+    //         // console.log(img.bitmap.data[j*img.bitmap.width + i - 1])
+    //         if(img.bitmap.data[j*img.bitmap.width + i + 2] == 0){
+    //             leftthreshold = i*177
+    //             break;
+    //         }
+    //     }
+    // }
+    
+
+    img.crop(0, upperthreshold, 177, lowerthreshold - upperthreshold + 1)
+    img.rotate(90)
+    // for (let i = 0; i < img.bitmap.height*img.bitmap.width; i++) {
+    //     if(img.bitmap.data[4*i] < 100){
+    //         upperthreshold = i/img.bitmap.width
+    //         // img.crop(0, upperthreshold - 1, 177, 177 - i%177)
+    //         break
+    //     }
+    // }
+    
+    // for (let i = img.bitmap.height*img.bitmap.width; i > 0; i--) {
+    //     if(img.bitmap.data[4*i - 2] < 100){
+    //         lowerthreshold = i/img.bitmap.width
+    //         // img.crop(0, 0, 177, 177 - i%177)
+    //         break
+    //     }
+    // }
+    // img.crop(0, upperthreshold, 177, lowerthreshold - upperthreshold + 1)
     img.write('modifiedlikesvg.jpg')
+
 })
+
